28fc6cd11fabd6559f84c09a4d01866bc295ab84
Alexis23030 <23030ah@hvhs.school.nz>
2026-02-01 22:05:53 +0000

    savey kinda works

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 
-  alert("Congratulations!\nYou got the script running.");
 
 
 const element = document.getElementById("myBtn");
 element.addEventListener("click", myFunction);
 
 function myFunction() {
-  document.getElementById("demo").innerHTML = "Hello World";
+  alert("Congratulations!\nYou got the script running.");
 }
