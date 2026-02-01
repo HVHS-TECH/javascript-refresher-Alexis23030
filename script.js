e01b69be18f61c887c250ef9566c6a8a06d8b099
Alexis23030 <23030ah@hvhs.school.nz>
2026-02-01 22:03:47 +0000

    save

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("Congratulations!\nYou got the script running.")
+
+  alert("Congratulations!\nYou got the script running.");
+
+
+const element = document.getElementById("myBtn");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
